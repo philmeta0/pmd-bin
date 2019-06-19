8839d0234ba4efbb32a12d3cdfb962c04d563cc2
= <charles@billable.io>
2019-06-19 18:04:56 -0500

    Added updated jar file and updated the download-pmd.sh script to use cp instead of mv.

--- download-pmd.sh
+++ download-pmd.sh
@@ -6,5 +6,5 @@ pushd dist > /dev/null || true
 # shellcheck disable=SC2154
 curl -SL "https://github.com/pmd/pmd/releases/download/pmd_releases%2F${npm_package_pmd_version}/pmd-bin-${npm_package_pmd_version}.zip" | jar xf /dev/stdin
 mv "pmd-bin-${npm_package_pmd_version}" pmd-bin
-mv "../lib-updates/pmd-apex-6.16.0-SNAPSHOT.jar" "pmd-bin/lib/pmd-apex-${npm_package_pmd_version}.jar"
+cp -f "../lib-updates/pmd-apex-6.16.0-SNAPSHOT.jar" "pmd-bin/lib/pmd-apex-${npm_package_pmd_version}.jar"
 popd > /dev/null || true
