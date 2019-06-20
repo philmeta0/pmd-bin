dad075fdbbe1cb2784f1da56c35c731872c8206a
Phil Meta <philmeta0@gmail.com>
2019-06-20 16:48:12 -0400

    hardcoding pmd version

--- download-pmd.sh
+++ download-pmd.sh
@@ -4,7 +4,9 @@ rm -rf dist > /dev/null
 mkdir -p dist > /dev/null
 pushd dist > /dev/null || true
 # shellcheck disable=SC2154
-curl -SL "https://github.com/pmd/pmd/releases/download/pmd_releases%2F${npm_package_pmd_version}/pmd-bin-${npm_package_pmd_version}.zip" | jar xf /dev/stdin
-mv "pmd-bin-${npm_package_pmd_version}" pmd-bin
-cp -f "../lib-updates/pmd-apex-6.16.0-SNAPSHOT.jar" "pmd-bin/lib/pmd-apex-${npm_package_pmd_version}.jar"
+echo "~~~ downloading pmd from github"
+curl -SL "https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.15.0/pmd-bin-6.15.0.zip" | jar xf /dev/stdin
+mv "pmd-bin-6.15.0" pmd-bin
+echo "~~~ moving updated jar file"
+cp -f "../lib-updates/pmd-apex-6.16.0-SNAPSHOT.jar" "pmd-bin/lib/pmd-apex-6.15.0.jar"
 popd > /dev/null || true
